09ec6eff30aca3363f9bf9c5a192bf332aba815c
singha5 <arunjitsingh6@gmail.com>
2014-05-07 09:44:43 -0400

    johnny

---
+++ John/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+    
+    public static string Reverse(Char[] a)
+        {
+            char[]seq= name.ToCharArray();
+            string output=new string(seq);
+        }
+            static void Main(string[] args)
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
